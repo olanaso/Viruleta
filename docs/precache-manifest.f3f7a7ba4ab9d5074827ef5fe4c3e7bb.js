90b83a6b481a315037a1f3fc73184a7fc0beaab6
Erick Escalante Olano <ericks.escalanteolano@gmail.com>
2020-06-08 20:38:56 -0500

    add viruleta 3

---
+++ docs/precache-manifest.f3f7a7ba4ab9d5074827ef5fe4c3e7bb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21f2737159caf1d1dbe48c266ee5a43",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fbb5e7d9634174cf69f1",
+    "url": "./static/css/main.14b44125.chunk.css"
+  },
+  {
+    "revision": "f533ae58f0d63492e11d",
+    "url": "./static/js/2.3c49565c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3c49565c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb5e7d9634174cf69f1",
+    "url": "./static/js/main.f3c2177d.chunk.js"
+  },
+  {
+    "revision": "c3dccf02b23e7bedac79",
+    "url": "./static/js/runtime-main.40957d59.js"
+  },
+  {
+    "revision": "dab6940bded7e249b07ecc65426eaaad",
+    "url": "./static/media/010707105_prev.dab6940b.mp3"
+  },
+  {
+    "revision": "8a4c03403ba6ba65fa318c1189d1d407",
+    "url": "./static/media/Astronomia.8a4c0340.mp3"
+  },
+  {
+    "revision": "93436ef2edf897807ae7f2f09c5bc45c",
+    "url": "./static/media/apilar.93436ef2.svg"
+  },
+  {
+    "revision": "1a4dfbf9a7b272b393bf175888d8b18f",
+    "url": "./static/media/button-3.1a4dfbf9.mp3"
+  },
+  {
+    "revision": "a1def28a59da436753c5384c6fb590a7",
+    "url": "./static/media/careless-whisper.a1def28a.mp3"
+  },
+  {
+    "revision": "8b46378caf310d48e743b13574e167d3",
+    "url": "./static/media/champions.8b46378c.mp3"
+  },
+  {
+    "revision": "78956a31eb03261acdadcf77681087c5",
+    "url": "./static/media/exorcist.78956a31.mp3"
+  },
+  {
+    "revision": "4d1ff35b6604f8690708bb97459bdab0",
+    "url": "./static/media/felicidades-animadas.4d1ff35b.gif"
+  },
+  {
+    "revision": "8b46caef18025ff54c6132798c85049d",
+    "url": "./static/media/ficha.8b46caef.svg"
+  },
+  {
+    "revision": "014842b604b118fe7b4adab8712c1223",
+    "url": "./static/media/gana.014842b6.svg"
+  },
+  {
+    "revision": "ca00c34eec8f40d669728e305a8097b0",
+    "url": "./static/media/guile-theme.ca00c34e.mp3"
+  },
+  {
+    "revision": "1b03af4cf5b6804bf85522b47ba9d1aa",
+    "url": "./static/media/mario-coin.1b03af4c.mp3"
+  },
+  {
+    "revision": "c0a78269d1aa0d1c18e9266563246644",
+    "url": "./static/media/negocio.c0a78269.svg"
+  },
+  {
+    "revision": "5b035fb1e20f8eab0aee259398ad20be",
+    "url": "./static/media/negros.5b035fb1.gif"
+  },
+  {
+    "revision": "fb98aa57f8d22ba391de79d4df9b3b7f",
+    "url": "./static/media/psycho.fb98aa57.mp3"
+  },
+  {
+    "revision": "0c8c78570b022513f20903f32bd51805",
+    "url": "./static/media/xfile.0c8c7857.mp3"
+  }
+]);
